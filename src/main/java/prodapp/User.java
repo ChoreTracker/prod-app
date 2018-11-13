6a2614d201e1b8908b59da9f84597ad002d65eba
Don <dmiesle@mac.com>
2018-11-13 17:30:12 -0500

    added user account and updated password to handle new security framework

--- src/main/java/prodapp/User.java
+++ src/main/java/prodapp/User.java
@@ -10,6 +10,7 @@ import javax.persistence.ManyToMany;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,6 +19,7 @@ import org.springframework.ui.Model;
 @Entity
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity( securedEnabled = true )
 public class User extends WebSecurityConfigurerAdapter{
 	@Id
 	@GeneratedValue
@@ -28,11 +30,13 @@ public class User extends WebSecurityConfigurerAdapter{
 	private Collection<Mission> missions;
 
 	@Autowired
-	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) 
+	protected void configureAuth(AuthenticationManagerBuilder auth) 
 			throws Exception {
-				auth.inMemoryAuthentication().withUser("Admin").password("admin")
-				.roles("USER", "ADMIN");
+				auth.inMemoryAuthentication().withUser("admin").password("{noop}admin")
+				.roles("USER", "ADMIN")
+				.and().withUser("user").password("{noop}user").roles("USER");
 			}
+
 	
 	public User() {
 
