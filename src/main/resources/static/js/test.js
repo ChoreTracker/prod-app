87642bfcc9284b65460edff092d9dcb9b2332095
hmcka <hethur@hmcka.com>
2018-12-01 08:29:26 -0500

    added some image icons and played with user.html

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,3 +1,4 @@
+//A general Modal Box
 // Get the modal
 var modal = document.getElementById('myModal');
 
@@ -18,8 +19,84 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+
+//The tab text.
+function myMissions() {
+    var individualMissions2 = document.getElementById("individualMissions2");
+
+    //get current value of the display property
+    var displaySetting = individualMissions2.style.display;
+
+    if (displaySetting == 'block') {
+        //var is visible. hide it.
+        individualMissions2.style.display = 'none';
+    }
+    else {
+        //var is hidden.show it.
+        individualMissions2.style.display = 'block';
+    }
+}
+
+function unclaimedMissions() {
+    var unclaimedlMissions2 = document.getElementById("unclaimedMissions2");
+
+    //get current value of the display property
+    var displaySetting = unclaimedlMissions2.style.display;
+
+    if (displaySetting == 'block') {
+        //var is visible. hide it.
+        unclaimedMissions2.style.display = 'none';
+    }
+    else {
+        //var is hidden.show it.
+        unclaimedMissions2.style.display = 'block';
+    }
+}
+
+function allMissions() {
+    var allMissions2 = document.getElementById("allMissions2");
+
+    //get current value of the display property
+    var displaySetting = allMissions2.style.display;
+
+    if (displaySetting == 'block') {
+        //var is visible. hide it.
+        allMissions2.style.display = 'none';
+    }
+    else {
+        //var is hidden.show it.
+        allMissions2.style.display = 'block';
+    }
+}
+
+
+
+// Pop-up sub-nav
+function goToSelectedPopsUp() {
+    var gotoSelected = document.getElementById("sub-nav");
+
+    //get current value of the display property
+    var displaySetting = gotoSelected.style.display;
+
+    if (displaySetting == 'block') {
+        //var is visible. hide it.
+        gotoSelected.style.display = 'none';
+    }
+    else {
+        //var is hidden.show it.
+        gotoSelected.style.display = 'block';
+    }
+}
+
+
+
+
+
+//Adding a new mission
+
